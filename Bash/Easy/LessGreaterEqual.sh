7e7f5fd2276cb177c36fbcce621c4834a8c04c86
Kamal Hussein <kamalinuxx@gmail.com>
2022-10-23 00:06:14 +0200

    Less Greater Equal to

---
+++ Bash/Easy/LessGreaterEqual.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Given two integers,  and , identify whether  or  or .
+
+read x
+read y
+
+if [ $x -lt $y ]
+then
+    echo "X is less than Y"
+elif [ $x -gt $y ]
+then
+    echo "X is greater than Y"
+else
+	echo "X is equal to Y"
+fi
